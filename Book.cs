4cca3f612a2c53aded565f183e43d387a9ba0b8f
Kover Marcell <1228marci@gmail.com>
2025-03-07 12:37:03 +0100

    kesz a feladat

--- Book.cs
+++ Book.cs
@@ -15,13 +15,32 @@ namespace Books
             this.releaseYear = releaseYear;
         }
 
-        public string Title {get => title; set => title = value;}
-        public string Author {get => author; set => author = value;}
-        public int PageCount {get => pageCount; set => pageCount = value;}
-        public int ReleaseYear {get => releaseYear; set => releaseYear = value;}
+        public string Title {get => title;}
+        public string Author {get => author;}
+        public int PageCount {get => pageCount;}
+        public int ReleaseYear {get => releaseYear;}
 
-        
+        public string GetBookByTitle(Book[] books, string title){
+            foreach (var book in books)
+            {
+                if (book.Title == title)
+                {
+                    return book.ToString();
+                }
+            }
+            return "Nincs ilyen könyv!";
+        }
 
+        public void EditBookData(){
+            System.Console.WriteLine("Kérem adja meg a könyv új címét:");
+            this.title = System.Console.ReadLine();
+            System.Console.WriteLine("Kérem adja meg a könyv új szerzőjét:");
+            this.author = System.Console.ReadLine();
+            System.Console.WriteLine("Kérem adja meg a könyv új oldalszámát:");
+            this.pageCount = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("Kérem adja meg a könyv új megjelenési évét:");
+            this.releaseYear = int.Parse(System.Console.ReadLine());
+        }
         public override string ToString()
         {
             return $"{this.author}: {this.title} - {this.pageCount} oldal. Megjelenés: {this.releaseYear}";
--- Program.cs
+++ Program.cs
@@ -9,11 +9,12 @@ namespace Books
             Book b1 = new Book("A kis herceg", "Antoine de Saint-Exupéry", 96, 1943);
             Book b2 = new Book("A pal utcai fiuk", "Molnár Ferenc", 256, 1907);
             Book b3 = new Book("Egri csillagok", "Gárdonyi Géza", 400, 1901);
-            Console.WriteLine(b1);
-            Console.WriteLine(b2);
-            Console.WriteLine(b3);
             Book[] books = { b1, b2, b3 };
-            b1.GetBookTitle(books);
+            Console.WriteLine(b1.GetBookByTitle(books, "A kis herceg"));
+            Console.WriteLine(b1.GetBookByTitle(books, "A pal utcai fiuk"));
+            Console.WriteLine(b1.GetBookByTitle(books, "Egri csillagok"));
+            b1.EditBookData();
+            System.Console.WriteLine(b1);
         }
     }
 }
